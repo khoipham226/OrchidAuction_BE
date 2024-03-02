b8763016c13f8bc319f9206c73227741de467112
khoipham226 <113128599+khoipham226@users.noreply.github.com>
2024-03-02 20:42:16 +0700

    fix

--- testdemo/BusinessLayer/ResponseModels/LoginResponseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace STARAS.BusinessLayer.ResponseModels
-{
-    public class LoginResponseModel
-    {
-        public AccountResponseModel Account { get; set; }
-        public TokenModel Token { get; set; }
-        public int StoreID { get; set; }
-    }
-}
